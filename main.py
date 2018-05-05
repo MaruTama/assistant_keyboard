b9a495cf3e9ce57aa4e2e073797c70781c95246b
MaruTama <b1403525@planet.kanazawa-it.ac.jp>
2018-05-05 19:27:38 +0900

    アシスタント処理のクラス化及び認識文章の表示を行うように変更した

--- main.py
+++ main.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Run a recognizer using the Google Assistant Library with button support.
+"""Run a recognizer using the Google Assistant Library.
 
 The Google Assistant Library has direct access to the audio API, so this Python
 code doesn't need to record audio. Hot word detection "OK, Google" is supported.
@@ -21,13 +21,19 @@ code doesn't need to record audio. Hot word detection "OK, Google" is supported.
 It is available for Raspberry Pi 2/3 only; Pi Zero is not supported.
 """
 
+"""
+ボタンでも音声コマンドでも認識するようにした
+"""
+
 import logging
 import platform
+import subprocess
 import sys
 import threading
 
 import aiy.assistant.auth_helpers
 from aiy.assistant.library import Assistant
+import aiy.audio
 import aiy.voicehat
 from google.assistant.library.event import EventType
 
@@ -67,6 +73,7 @@ class MyAssistant(object):
 
     def _process_event(self, event):
         status_ui = aiy.voicehat.get_status_ui()
+        # 初期設定完了後
         if event.type == EventType.ON_START_FINISHED:
             status_ui.status('ready')
             self._can_start_conversation = True
@@ -76,19 +83,37 @@ class MyAssistant(object):
                 print('Say "OK, Google" or press the button, then speak. '
                       'Press Ctrl+C to quit...')
 
+		# 音声待ち
         elif event.type == EventType.ON_CONVERSATION_TURN_STARTED:
             self._can_start_conversation = False
             status_ui.status('listening')
+        # 音声認識完了後
+        elif event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED and event.args:
+            print('You said:', event.args['text'])
+            text = event.args['text'].lower()
+            if text == 'power off':
+                # 応答停止
+                assistant.stop_conversation()
+                self.power_off_pi()
+            elif text == 'reboot':
+                assistant.stop_conversation()
+                self.reboot_pi()
+            elif text == 'ip address':
+                assistant.stop_conversation()
+                self.say_ip()
 
+		# 音声認識中
         elif event.type == EventType.ON_END_OF_UTTERANCE:
             status_ui.status('thinking')
 
+		# 音声コマンド(ok google)待ち
         elif (event.type == EventType.ON_CONVERSATION_TURN_FINISHED
               or event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT
               or event.type == EventType.ON_NO_RESPONSE):
             status_ui.status('ready')
             self._can_start_conversation = True
 
+        # assistantが起動できなかったとき
         elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
             sys.exit(1)
 
@@ -100,6 +125,17 @@ class MyAssistant(object):
         if self._can_start_conversation:
             self._assistant.start_conversation()
 
+    def power_off_pi(self):
+        aiy.audio.say('Good bye!')
+        subprocess.call('sudo shutdown now', shell=True)
+
+    def reboot_pi(self):
+        aiy.audio.say('See you in a bit!')
+        subprocess.call('sudo reboot', shell=True)
+
+    def say_ip(self):
+        ip_address = subprocess.check_output("hostname -I | cut -d' ' -f1", shell=True)
+        aiy.audio.say('My IP address is %s' % ip_address.decode('utf-8'))
 
 def main():
     if platform.machine() == 'armv6l':
